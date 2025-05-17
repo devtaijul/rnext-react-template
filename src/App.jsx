69b94eb156df73d54ef9353fe425acec55830c39
Taijul Islam <devtaijul@gmail.com>
2025-05-17 12:58:06 +0600

    rnext-react-template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-4xl text-center font-bold text-red-800">
+      <h1 className="text-4xl text-center font-bold text-red-800">
+        Hello Vite + React!
+      </h1>
+      <div className="text-2xl text-center font-bold text-blue-800">
+        <p>Use this template as startup of rnext assigmentg</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/icons/config.js
@@ -0,0 +1,13 @@
+import { ArrowDown } from "./ArrowDown";
+import { ArrowIcon } from "./ArrowIcon";
+import { ArrowUp } from "./ArrowUp";
+import { UsersIcon } from "./UsersIcon";
+
+const ICON_NAMES = {
+  arrow_icon: ArrowIcon,
+  users_icon: UsersIcon,
+  arrow_up: ArrowUp,
+  arrow_down: ArrowDown,
+};
+
+export { ICON_NAMES };
